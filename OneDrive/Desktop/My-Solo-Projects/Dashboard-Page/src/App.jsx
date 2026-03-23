87e22a37c4f13b2691b57fec9d9fe66884ece87c
jamila ait <hamouni5099@gmail.com>
2026-03-23 08:18:20 +0100

    Add dark mode toggle, improve UI styling..

--- OneDrive/Desktop/My-Solo-Projects/Dashboard-Page/src/App.jsx
+++ OneDrive/Desktop/My-Solo-Projects/Dashboard-Page/src/App.jsx
@@ -8,12 +8,15 @@ import { useState } from "react";
 function App(){
 
   const [isOpen, setIsOpen]=useState(true)
+  const [darkMode, setDarkMode]=useState(false)
 
   return(
-    <div className="dashboard">
+    <div className={`dashboard ${darkMode ? "darkMode" : ""}`}>
       <Sidebar isOpen={isOpen} />
       <div className="main-content">
-        <Header toggleSidebar={()=>setIsOpen(!isOpen)}/>
+        <Header toggleSidebar={()=>setIsOpen(!isOpen)}
+          toggleDarkMode={()=>setDarkMode(!darkMode)}
+          darkMode={darkMode}/>
         <div className="stats">
           <StatCard title="Revenue" value="$12,450" />
           <StatCard title="Users" value="1,230" />
--- OneDrive/Desktop/My-Solo-Projects/Dashboard-Page/src/components/Header.jsx
+++ OneDrive/Desktop/My-Solo-Projects/Dashboard-Page/src/components/Header.jsx
@@ -1,11 +1,12 @@
+import {FaMoon, FaSun} from "react-icons/fa"
 
-
-function Header({toggleSidebar}){
+function Header({toggleSidebar, toggleDarkMode, darkMode}){
   return(
     <header className="header">
       <button onClick={toggleSidebar}>☰</button>
       <input type="text" placeholder="Search..."/>
       <div className="header-right">
+        <button onClick={toggleDarkMode}>{darkMode ? <FaSun /> : <FaMoon />}</button>
         <span>🔔</span>
         <span>👤 Jamila</span>
       </div>
